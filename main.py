5a8bd7967cb8f4501155c5437ee0132488163093
Go-YoungMin <101621008+Go-YM@users.noreply.github.com>
2023-12-03 10:01:24 +0900

    main.py Staff, User GUI 구현

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QMessageBox, QVBoxLayout, QHBoxLayout, QDialog
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtCore import Qt
-from db import check_login_credentials, register_new_user
+from db import check_login_User, check_login_Staff, register_new_user
 
 class MyWindow(QWidget):
     def __init__(self):
@@ -43,6 +43,7 @@ class MyWindow(QWidget):
         signup_button.clicked.connect(self.show_registration_window)
 
         self.user_window = UserWindow()
+        self.staff_window = StaffWindow()
         self.registration_window = RegistrationWindow(self)
 
         self.show()
@@ -51,11 +52,14 @@ class MyWindow(QWidget):
         id_text = self.id_input.text()
         pw_text = self.pw_input.text()
 
-        if check_login_credentials(id_text, pw_text):
+        if check_login_User(id_text, pw_text):
             QMessageBox.information(self, "로그인 성공", "User 로그인 성공!")
             self.hide()
             self.show_user_window()
-        #elif Staff 로그인
+        elif check_login_Staff(id_text, pw_text):
+            QMessageBox.information(self,"로그인 성공", "Staff 로그인 성공!")
+            self.hide()
+            self.show_staff_window()
         else:
             QMessageBox.warning(self, "로그인 실패", "ID와 PW가 일치하지 않습니다. 다시 로그인해주세요.")
             self.clear_inputs()
@@ -81,6 +85,9 @@ class MyWindow(QWidget):
     
     def show_user_window(self):
         self.user_window.show()
+        
+    def show_staff_window(self):
+        self.staff_window.show()
 
 
 
@@ -145,9 +152,11 @@ class RegistrationWindow(QDialog):
 
         
         
+
 class UserWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.sort_count = {'price': 0, 'distance': 0, 'age': 0}
 
         self.initUI()
 
@@ -160,9 +169,119 @@ class UserWindow(QWidget):
         self.banner_label.setStyleSheet("background-color: #4CAF50; color: white;")
         self.banner_label.setAlignment(Qt.AlignCenter)
         self.banner_label.setFont(QFont('Arial', 20))
-        self.banner_label.setText("중고차 매매 시스템 - 사용자 창")
+        self.banner_label.setText("중고차 매매 시스템 - User")
 
-        # 사용자 창에서 필요한 요소들 추가
+        image_path = 'C:\\Users\\user\\Desktop\\DBS_TermProject\\Database 생성\\image\\01라3164'  # 추후 db리스트로 불러오기
+        image_label = QLabel(self)
+        pixmap = QPixmap(image_path)
+        scaled_pixmap = pixmap.scaled(400, 300, Qt.KeepAspectRatio) 
+        image_label.setPixmap(scaled_pixmap)
+        image_label.setGeometry(20, 100, 400, 300)
+
+        reserve_button = QPushButton('예약하기', self)
+        reserve_button.setGeometry(350, 550, 100, 30)
+        reserve_button.clicked.connect(self.reserve_button_clicked)
+
+        price_sort_button = QPushButton('가격순 정렬', self)
+        distance_sort_button = QPushButton('주행거리순 정렬', self)
+        age_sort_button = QPushButton('연식순 정렬', self)
+
+        left_arrow_button = QPushButton('←', self)
+        right_arrow_button = QPushButton('→', self)
+
+        button_width = 240
+        button_height = 20
+        arrow_button_width = 30
+
+        price_sort_button.clicked.connect(self.price_sort_clicked)
+        distance_sort_button.clicked.connect(self.distance_sort_clicked)
+        age_sort_button.clicked.connect(self.age_sort_clicked)
+
+        left_arrow_button.clicked.connect(self.left_arrow_clicked)
+        right_arrow_button.clicked.connect(self.right_arrow_clicked)
+
+        price_sort_button.setGeometry(40, 60, button_width, button_height)
+        distance_sort_button.setGeometry(280, 60, button_width, button_height)
+        age_sort_button.setGeometry(520, 60, button_width, button_height)
+
+        left_arrow_button.setGeometry(0, 60, arrow_button_width, button_height)
+        right_arrow_button.setGeometry(770, 60, arrow_button_width, button_height)
+
+    def price_sort_clicked(self):
+        self.sort_clicked('price')
+
+    def distance_sort_clicked(self):
+        self.sort_clicked('distance')
+
+    def age_sort_clicked(self):
+        self.sort_clicked('age')
+
+    def sort_clicked(self, category):
+        self.sort_count[category] += 1
+
+        if self.sort_count[category] % 2 == 1:
+            print(f'{category}을(를) 내림차순으로 정렬합니다.')
+        else:
+            print(f'{category}을(를) 오름차순으로 정렬합니다.')
+
+    def left_arrow_clicked(self):
+        print('왼쪽 화살표 버튼이 클릭되었습니다.')
+
+    def right_arrow_clicked(self):
+        print('오른쪽 화살표 버튼이 클릭되었습니다.')
+
+    def reserve_button_clicked(self):
+        print('예약하기 버튼이 클릭되었습니다.')
+
+        
+        
+        
+        
+class StaffWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 800, 600)
+        self.setWindowTitle('DBS: Term Project - Staff Window')
+
+        self.banner_label = QLabel(self)
+        self.banner_label.setGeometry(0, 0, self.width(), 50)
+        self.banner_label.setStyleSheet("background-color: #4CAF50; color: white;")
+        self.banner_label.setAlignment(Qt.AlignCenter)
+        self.banner_label.setText("중고차 매매 시스템 - Staff")
+
+        register_button = QPushButton('Register', self)
+        delete_button = QPushButton('Delete', self)
+        show_button = QPushButton('Show', self)
+        
+        button_width = 200
+        button_height = 100
+        register_button.setFixedSize(button_width, button_height)
+        delete_button.setFixedSize(button_width, button_height)
+        show_button.setFixedSize(button_width, button_height)
+
+        register_button.clicked.connect(lambda: self.button_clicked(register_button))
+        delete_button.clicked.connect(lambda: self.button_clicked(delete_button))
+        show_button.clicked.connect(lambda: self.button_clicked(show_button))
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(register_button)
+        hbox.addWidget(delete_button)
+        hbox.addWidget(show_button)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+    def button_clicked(self, button):
+        print(f'{button.text()} 버튼이 눌렸습니다.')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
